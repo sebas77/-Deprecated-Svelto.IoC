8fb0ca40406f84fb519a5123064cb4b8406816a4
sebas77 <ssense@tiscali.it>
2017-05-29 15:11:01 +0100

    update to the latest version

---
+++ IoC/Extensions/Factories/GameObjectFactory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Svelto.IoC
+{
+    public class GameObjectFactory : Context.GameObjectFactory
+    {
+        public GameObjectFactory(IContainer container):base()
+        {
+            _container = container;
+        }
+
+        override public GameObject Build(GameObject prefab)
+        {
+            var copy =  base.Build(prefab);
+            var monobehaviours = copy.GetComponentsInChildren<MonoBehaviour>();
+
+            for (int i = 0; i < monobehaviours.Length; i++)
+                _container.Inject(monobehaviours[i]);
+
+            return copy;
+        }
+        
+
+        IContainer _container;
+    }
+}
---
+++ IoC/Extensions/Factories/MonoBehaviourFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Svelto.IoC
+{
+    public class MonoBehaviourFactory : Context.MonoBehaviourFactory
+    {
+        public MonoBehaviourFactory(IContainer container):base()
+        {
+            _container = container;
+        }
+
+        override public M Build<M>(Func<M> constructor)
+        {
+            var copy =  base.Build(constructor);
+            
+            _container.Inject(copy);
+
+            return copy;
+        }
+ 
+        IContainer _container;
+    }
+}
