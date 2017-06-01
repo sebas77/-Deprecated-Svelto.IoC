ea8227d23af4b9ece31423814880cc3523391211
sebas77 <ssense@tiscali.it>
2017-06-01 16:22:07 +0100

    this git submodule logic is a mess

---
+++ Utilities/WeakAction.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Svelto.DataStructures;
+
+public class WeakAction<T1, T2> : WeakAction
+{
+    public WeakAction(Action<T1, T2> listener)
+        : base(listener.Target, listener.Method)
+    {
+    }
+
+    public void Invoke(T1 data1, T2 data2)
+    {
+        if (ObjectRef.IsValid)
+            Method.Invoke(ObjectRef.Target, new object[] { data1, data2 });
+        else
+            Utility.Console.LogWarning("Target of weak action has been garbage collected");
+    }
+}
+
+public class WeakAction<T> : WeakActionBase
+{
+    public WeakAction(Action<T> listener)
+        : base(listener.Target, listener.Method)
+    {
+    }
+
+    public void Invoke(T data)
+    {
+        Invoke_Internal(data);
+    }
+
+    void Invoke_Internal(T data)
+    {
+        if (ObjectRef.IsValid)
+            Method.Invoke(ObjectRef.Target, new object[] {data});
+        else
+            Utility.Console.LogWarning("Target of weak action has been garbage collected");
+    }
+}
+
+public class WeakAction : WeakActionBase
+{
+    public WeakAction(Action listener) 
+        : base(listener)
+    {
+    }
+
+    public WeakAction(object listener, MethodInfo method) 
+        : base(listener, method)
+    {
+    }
+    
+    public void Invoke()
+    {
+        if (ObjectRef.IsValid)
+            Method.Invoke(ObjectRef.Target, null);
+        else
+            Utility.Console.LogError("Target of weak action has been garbage collected");
+    }
+}
+
+public abstract class WeakActionBase
+{
+    protected readonly WeakReference<object> ObjectRef;
+    protected readonly MethodInfo Method;
+    
+    public bool IsValid
+    {
+        get { return ObjectRef.IsValid; }
+    }
+
+    public WeakActionBase(Action listener)
+        : this(listener.Target, listener.Method)
+    { }
+
+    public WeakActionBase(object listener, MethodInfo method)
+    {
+        ObjectRef = new WeakReference<object>(listener);
+        
+        Method = method;
+
+        if (method.DeclaringType.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false).Length != 0)
+            throw new ArgumentException("Cannot create weak event to anonymous method with closure.");
+    }
+}
+
---
+++ Utilities/WeakActionStruct.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//careful, you must handle the destruction of the GCHandles!
+
+namespace BetterWeakEvents
+{
+    public struct WeakAction<T1, T2> : IEquatable<WeakAction<T1, T2>>
+    {
+        public WeakAction(Action<T1, T2> listener)
+        {
+            ObjectRef = GCHandle.Alloc(listener.Target, GCHandleType.Weak);
+
+#if !NETFX_CORE
+            Method = listener.Method;
+#else
+            Method = listener.GetMethodInfo();
+#endif
+        }
+
+        public bool Invoke(T1 data1, T2 data2)
+        {
+            if (ObjectRef.IsAllocated && ObjectRef.Target != null)
+            {
+                Method.Invoke(ObjectRef.Target, new object[] { data1, data2 });
+                return true;
+            }
+
+            Release();
+            return false;
+        }
+
+        public bool Equals(WeakAction<T1, T2> other)
+        {
+            return (Method.Equals(other.Method));
+        }
+
+        public void Release()
+        {
+            ObjectRef.Free();
+        }
+
+        readonly GCHandle ObjectRef;
+        readonly MethodInfo Method;
+    }
+
+    public struct WeakAction<T> : IEquatable<WeakAction<T>>
+    {
+        public WeakAction(Action<T> listener)
+        {
+            ObjectRef = GCHandle.Alloc(listener.Target, GCHandleType.Weak);
+#if !NETFX_CORE
+            Method = listener.Method;
+#else
+            Method = listener.GetMethodInfo();
+#endif
+        }
+
+        public bool Invoke(T data)
+        {
+            if (ObjectRef.IsAllocated && ObjectRef.Target != null)
+            {
+                Method.Invoke(ObjectRef.Target, new object[] { data });
+                return true;
+            }
+
+            Release();
+            return false;
+        }
+
+        public bool Equals(WeakAction<T> other)
+        {
+            return (Method.Equals(other.Method));
+        }
+
+        public void Release()
+        {
+            ObjectRef.Free();
+        }
+
+        readonly GCHandle ObjectRef;
+        readonly MethodInfo Method;
+    }
+
+    public struct WeakAction : IEquatable<WeakAction>
+    {
+        public WeakAction(Action listener)
+        {
+            ObjectRef = GCHandle.Alloc(listener.Target, GCHandleType.Weak);
+#if !NETFX_CORE
+            Method = listener.Method;
+#else
+            Method = listener.GetMethodInfo();
+#endif
+        }
+
+        public bool Invoke()
+        {
+            if (ObjectRef.IsAllocated && ObjectRef.Target != null)
+            {
+                Method.Invoke(ObjectRef.Target, null);
+                return true;
+            }
+
+            Release();
+            return false;
+        }
+
+        public bool Equals(WeakAction other)
+        {
+            return (Method.Equals(other.Method));
+        }
+
+        public void Release()
+        {
+            ObjectRef.Free();
+        }
+
+        readonly GCHandle ObjectRef;
+        readonly MethodInfo Method;
+    }
+
+
+}
